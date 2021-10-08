5a4500f8ab69d54eea1061d3cb97877b3dd4e5cf
xuhaoming <1019103752@qq.com>
2021-10-08 10:29:59 +0800

    修改数据库初始化脚本

--- doc/mysql/init.sql
+++ doc/mysql/init.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE `hami` CHARACTER SET 'utf8mb4';
 
-CREATE TABLE `hami_order` (
+CREATE TABLE `hami`.`hami_order` (
                             `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '自增主键',
                             `order_no` varchar(50) NOT NULL COMMENT '订单号',
                             `user_id` bigint(20) NOT NULL COMMENT '用户ID',
@@ -9,6 +9,6 @@ CREATE TABLE `hami_order` (
                             `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
                             PRIMARY KEY (`id`),
                             KEY `uk_order_no` (`order_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-insert into `hami_order`(order_no, user_id, price) VALUE('000001', 1, 100), ('000002', 2, 99.88)
+insert into `hami`.`hami_order`(order_no, user_id, price) VALUE('000001', 1, 100), ('000002', 2, 99.88);
